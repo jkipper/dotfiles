0afd08112122c990076a747edea3aba27f50d483
Justin Kipper <justin.kipper94@gmail.com>
2021-12-15 21:24:20 +0100

    Fix deprecation warning

--- nvim_new/lua/lsp_setup.lua
+++ nvim_new/lua/lsp_setup.lua
@@ -1,100 +1,104 @@
-local lsp = require "lspconfig"
-local installer = require "nvim-lsp-installer"
+local lsp = require("lspconfig")
+local installer = require("nvim-lsp-installer")
 local servers = {
-    "jedi_language_server", "sumneko_lua", "dockerls", "jsonls", "clangd"
+	"jedi_language_server",
+	"sumneko_lua",
+	"dockerls",
+	"jsonls",
+	"clangd",
 }
-local cmp = require 'cmp'
-local lspkind = require('lspkind')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
 local try_require = function(module)
-    local status, lfs = pcall(require, module)
-    if status then return lfs end
+	local status, lfs = pcall(require, module)
+	if status then
+		return lfs
+	end
 end
 
 local init_cmp = function()
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                vim.fn["vsnip#anonymous"](args.body)
-            end
-        },
-        formatting = {
-            format = lspkind.cmp_format({with_text = true, maxwidth = 50})
-        },
-        mapping = {
-            ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
-            ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
-            ['<Tab>'] = cmp.mapping.select_next_item(),
-            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-            ["<CR>"] = cmp.mapping.confirm({select = true}),
-            ["<ESC>"] = cmp.mapping.abort()
-        },
-        sources = cmp.config.sources({{name = 'nvim_lsp'}, {name = 'vsnip'}},
-                                     {name = 'buffer'}),
-        experimental = {native_menu = false, ghost_test = true}
-    })
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				vim.fn["vsnip#anonymous"](args.body)
+			end,
+		},
+		formatting = {
+			format = lspkind.cmp_format({ with_text = true, maxwidth = 50 }),
+		},
+		mapping = {
+			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+			["<Tab>"] = cmp.mapping.select_next_item(),
+			["<S-Tab>"] = cmp.mapping.select_prev_item(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+			["<ESC>"] = cmp.mapping.abort(),
+		},
+		sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "vsnip" } }, { name = "buffer" }),
+		experimental = { native_menu = false, ghost_test = true },
+	})
 end
 
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-    local opts = {noremap = true, silent = true}
+	local opts = { noremap = true, silent = true }
 
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>Trouble lsp_references<CR>', opts)
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
-                   opts)
-    buf_set_keymap('n', '<leader>ff', '<cmd>lua vim.lsp.buf.formatting()<CR>',
-                   opts)
-
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>Trouble lsp_references<CR>", opts)
+	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "<leader>ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
-                                                                     .protocol
-                                                                     .make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 init_cmp()
 
 for _, server in ipairs(servers) do
-    local server_available, requested_server = installer.get_server(server)
-    if server_available then
-        requested_server:on_ready(function()
-            requested_server:setup({
-                settings = try_require(server),
-                on_attach = on_attach,
-                capabilities = capabilities
-            })
-        end)
-        if not requested_server:is_installed() then
-            requested_server:install()
-        end
-    end
+	local server_available, requested_server = installer.get_server(server)
+	if server_available then
+		requested_server:on_ready(function()
+			requested_server:setup({
+				settings = try_require(server),
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+		end)
+		if not requested_server:is_installed() then
+			requested_server:install()
+		end
+	end
 end
 
 local null_on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ff',
-                                '<cmd>lua vim.lsp.buf.formatting()<CR>',
-                                {noremap = true, silent = true})
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<leader>ff",
+		"<cmd>lua vim.lsp.buf.formatting()<CR>",
+		{ noremap = true, silent = true }
+	)
 end
 
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 local formatters = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 null_ls.setup({
-    sources = {
-        formatters.black,
-        formatters.lua_format, formatters.clang_format,
-        diagnostics.flake8
-    }
-})
+	sources = {
+		formatters.black,
+		formatters.stylua,
+		formatters.clang_format,
+		diagnostics.flake8,
+		code_actions.gitsigns,
+	},
 
-require("lspconfig")["null-ls"].setup({
-    on_attach = null_on_attach,
-    autostart = true
+	on_attach = null_on_attach,
+	autostart = true,
 })
-
--- nvim_new/lua/plugins.lua
+++ nvim_new/lua/plugins.lua
@@ -1,120 +1,151 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({
-        'git', 'clone', '--depth', '1',
-        'https://github.com/wbthomason/packer.nvim', install_path
-    })
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
-vim.cmd [[packadd packer.nvim]]
+
+vim.cmd([[packadd packer.nvim]])
+
 return require("packer").startup(function(use)
-    use {"wbthomason/packer.nvim"}
-    use {
-        "kyazdani42/nvim-tree.lua",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require"nvim-tree".setup {filter = {custom = {".git", ".pyc$"}}}
-        end
-    }
-    use "tpope/vim-fugitive"
-    use {
-        "lewis6991/gitsigns.nvim",
-        requires = {'nvim-lua/plenary.nvim'},
-        config = function() require'gitsigns'.setup() end
-    }
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("nvim-treesitter.configs").setup {
-                ensure_installed = "maintained",
-                highlight = {enable = true},
-                tree_docs = {enable = true}
-            }
-        end
-    }
-    use "danilo-augusto/vim-afterglow"
-    use {"sonph/onehalf", rtp = 'vim'}
-    use 'folke/tokyonight.nvim'
-    use "rmehri01/onenord.nvim"
-    use "joshdick/onedark.vim"
-    use "voldikss/vim-floaterm"
+	use({ "wbthomason/packer.nvim" })
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("nvim-tree").setup({ filter = { custom = { ".git", ".pyc$" } } })
+		end,
+	})
+	use("tpope/vim-fugitive")
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = "maintained",
+				highlight = { enable = true },
+				tree_docs = { enable = true },
+			})
+		end,
+	})
+	use("danilo-augusto/vim-afterglow")
+	use({ "sonph/onehalf", rtp = "vim" })
+	use("folke/tokyonight.nvim")
+	use("rmehri01/onenord.nvim")
+	use("joshdick/onedark.vim")
+	use("voldikss/vim-floaterm")
 
-    use 'ggandor/lightspeed.nvim'
-    use {
-        "terrortylor/nvim-comment",
-        config = function() require("nvim_comment").setup() end
-    }
-    use {"plasticboy/vim-markdown", requires = "godlygeek/tabular"}
-    use {
-        'iamcco/markdown-preview.nvim',
-        run = function() vim.fn['mkdp#util#install']() end,
-        ft = {'markdown'}
-    }
-    use {
-        "famiu/feline.nvim",
-        opt = false,
-        config = function() require"feline".setup {} end
-    }
+	use("ggandor/lightspeed.nvim")
+	use({
+		"terrortylor/nvim-comment",
+		config = function()
+			require("nvim_comment").setup()
+		end,
+	})
+	use({ "plasticboy/vim-markdown", requires = "godlygeek/tabular" })
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		ft = { "markdown" },
+	})
+	use({
+		"famiu/feline.nvim",
+		opt = false,
+		config = function()
+			require("feline").setup({})
+		end,
+	})
 
-    use {
-        "akinsho/bufferline.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require"bufferline".setup {
-                options = {
-                    numbers = "none",
-                    separator_style = "thin",
-                    close_icon = "",
-                    offsets = {
-                        {
-                            filetype = "NvimTree",
-                            text = "File Explorer",
-                            text_align = "center"
-                        }
-                    }
-                }
-            }
-        end
-    }
-    use "tpope/vim-surround"
-    use {
-        "hrsh7th/nvim-cmp",
-        requires = {
-            "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline", "hrsh7th/cmp-vsnip", "hrsh7th/vim-vsnip",
-            "neovim/nvim-lspconfig", "williamboman/nvim-lsp-installer",
-            "rafamadriz/friendly-snippets", "jose-elias-alvarez/null-ls.nvim",
-            "onsails/lspkind-nvim"
-        }
-    }
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'},
-        },
-        config = function()
-            local trouble = require 'trouble.providers.telescope'
-            require'telescope'.setup({
-                defaults = {
-                    mappings = {
-                        i = {["<a-x>"] = trouble.open_with_trouble},
-                        n = {["<a-x>"] = trouble.open_with_trouble}
-                    }
-                },
-            })
-        end
-    }
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function() require("trouble").setup {} end
-    }
-    use "sickill/vim-pasta"
-    use "tommcdo/vim-exchange"
-    use "unblevable/quick-scope"
-    use {"glts/vim-radical", requires = {"glts/vim-magnum"}}
-    use "tpope/vim-repeat"
-    use "tpope/vim-projectionist"
-    if packer_bootstrap then require('packer').sync() end
+	use({
+		"akinsho/bufferline.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					numbers = "none",
+					separator_style = "thin",
+					close_icon = "",
+					offsets = {
+						{
+							filetype = "NvimTree",
+							text = "File Explorer",
+							text_align = "center",
+						},
+					},
+				},
+			})
+		end,
+	})
+	use("tpope/vim-surround")
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+			"neovim/nvim-lspconfig",
+			"williamboman/nvim-lsp-installer",
+			"rafamadriz/friendly-snippets",
+			"jose-elias-alvarez/null-ls.nvim",
+			"onsails/lspkind-nvim",
+		},
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+		},
+		config = function()
+			local trouble = require("trouble.providers.telescope")
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = { ["<a-x>"] = trouble.open_with_trouble },
+						n = { ["<a-x>"] = trouble.open_with_trouble },
+					},
+				},
+			})
+		end,
+	})
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({})
+		end,
+	})
+	use("sickill/vim-pasta")
+	use("tommcdo/vim-exchange")
+	use("unblevable/quick-scope")
+	use({ "glts/vim-radical", requires = { "glts/vim-magnum" } })
+	use("tpope/vim-repeat")
+	use("tpope/vim-projectionist")
+	use("glepnir/dashboard-nvim")
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({})
+		end,
+	})
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
