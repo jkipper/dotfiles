c7b6a50a7d386e36af814bcb1d659fce0f484f46
Justin Kipper <justin.kipper2@de.bosch.com>
2022-07-26 14:24:12 +0200

    Add toml formatter

--- .config/nvim/lua/statusline/init.lua
+++ .config/nvim/lua/statusline/init.lua
@@ -1,42 +1,42 @@
 return function(use)
-	use {
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("lualine").setup {
-				options = {
-					theme = "tokyonight",
-				},
-				extensions = { "nvim-tree", "fugitive" },
-			}
-		end,
-	}
+  use {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "tokyonight",
+        },
+        extensions = { "nvim-tree", "fugitive" },
+      }
+    end,
+  }
 
-	use {
-		"akinsho/bufferline.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("bufferline").setup {
-				options = {
-					numbers = "none",
-					separator_style = "thin",
-					close_icon = "",
-					offsets = {
-						{
-							filetype = "NvimTree",
-							text = "File Explorer",
-							text_align = "center",
-						},
-					},
-				},
-			}
+  use {
+    "akinsho/bufferline.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup {
+        options = {
+          numbers = "none",
+          separator_style = "thin",
+          close_icon = "",
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              text_align = "center",
+            },
+          },
+        },
+      }
 
-			local keymap_opts = { noremap = true, silent = true }
-			vim.keymap.set("n", "<A-,>", function()
-				require("bufferline").cycle(-1)
-			end, keymap_opts)
-			vim.keymap.set("n", "<A-.>", function()
-				require("bufferline").cycle(1)
-			end, keymap_opts)
-		end,
-	}
+      local keymap_opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "<A-,>", function()
+        require("bufferline").cycle(-1)
+      end, keymap_opts)
+      vim.keymap.set("n", "<A-.>", function()
+        require("bufferline").cycle(1)
+      end, keymap_opts)
+    end,
+  }
 end
