a5f9da01deea9ff184c23b3a3a7793c7356c7932
Justin Kipper <justin.kipper94@gmail.com>
2022-07-31 12:29:01 +0200

    Switch to neotree

--- .config/nvim/lua/completion/lsp_settings/sumneko_lua.lua
+++ .config/nvim/lua/completion/lsp_settings/sumneko_lua.lua
@@ -11,6 +11,9 @@ return {
 				"undefined-field",
 			},
 		},
+    workspace = {
+      library = vim.api.nvim_get_runtime_file("", true)
+    }
 	},
 	completion = { callSnippet = "Both", keywordSnippet = "Both" },
 }
--- .config/nvim/lua/navigation/file_tree.lua
+++ .config/nvim/lua/navigation/file_tree.lua
@@ -1,24 +1,42 @@
-vim.g.lua_tree_show_icons = {
-  ["git"] = 1,
-  ["folders"] = 0,
-  ["files"] = 0,
-}
 return function(use)
   use {
-    "kyazdani42/nvim-tree.lua",
-    requires = "kyazdani42/nvim-web-devicons",
+    "nvim-neo-tree/neo-tree.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
     config = function()
-      require("nvim-tree").setup {
-        reload_on_bufenter = true,
-        filters = {
-          custom = { ".git", ".pyc$" },
+      require "neo-tree".setup {
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_by_name = {
+              ".git",
+              "*.pyc$"
+            },
+            never_show = {
+              ".DS_Store"
+            },
+          },
+          follow_current_file = true,
+          window = {
+            mappings = {
+              ["F"] = "clear_filter"
+            }
+          }
         },
-        diagnostics = { enable = true },
-        view = { mappings = { list = { { key = "<C-e>", action = "close" } } } },
+        window = {
+          mappings = {
+            ["<C-V>"] = "open_vsplit",
+            ["<C-X>"] = "open_split"
+          }
+        }
       }
+
       local keymap_opts = { noremap = true, silent = true }
-      vim.keymap.set("n", "<C-e>", "<cmd>NvimTreeFindFileToggle<cr>", keymap_opts)
-      vim.keymap.set("n", "<leader>r", "<cmd>NvimTreeRefresh<cr>", keymap_opts)
-    end,
+      vim.keymap.set("n", "<C-e>", "<cmd>NeoTreeFocusToggle<cr>", keymap_opts)
+      vim.keymap.set("n", "<C-b>", "<cmd>NeoTreeFocusToggle buffers<cr>", keymap_opts)
+    end
   }
 end
