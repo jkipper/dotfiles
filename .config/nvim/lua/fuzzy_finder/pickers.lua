31bec0adc3d05fe7c810a4cb198d1953ff9ae301
Justin Kipper <justin.kipper94@gmail.com>
2022-07-31 16:28:38 +0200

    Add preview to dotfiles picker

--- .config/nvim/lua/fuzzy_finder/pickers.lua
+++ .config/nvim/lua/fuzzy_finder/pickers.lua
@@ -2,15 +2,16 @@ local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require "telescope.config".values
 
+local M = {}
+
 local dotfiles = function(opts)
   opts = opts or {}
   pickers.new(opts, {
     prompt_title = "dotfiles",
-    finder = finders.new_oneshot_job { "yadm", "list" },
-    sorter = conf.file_sorter(opts)
+    finder = finders.new_oneshot_job({ "yadm", "list" }, {}),
+    sorter = conf.file_sorter(opts),
+    previewer = conf.file_previewer(opts)
   }):find()
 end
-
-local pickers = {}
-pickers.dotfiles = dotfiles
-return pickers
+M.dotfiles = dotfiles
+return M
