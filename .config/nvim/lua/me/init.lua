b6d5a4826fe461aec09008956882504c0b8b03c8
jkipper <justin.kipper94@gmail.com>
2022-09-05 19:55:24 +0200

    Fix some missing inits

--- .config/nvim/lua/me/init.lua
+++ .config/nvim/lua/me/init.lua
@@ -2,8 +2,14 @@ function R(module_name)
     require("plenary.reload").reload_module(module_name)
 end
 
+local dev_plugin = os.getenv "plugin_location"
+if dev_plugin ~= nil then
+    vim.opt.runtimepath:append(dev_plugin)
+end
+
 require "me.core.config"
 require("me.core.pack").load_plugins()
 require("me.core.pack").setup_reload()
 require "me.mini_tools"
 require "me.keymaps"
+require "me.core.events"
