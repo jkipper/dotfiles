1901b8a78fc36ca1dbd1fa8a4bb25e7326ddb140
jkipper <justin.kipper94@gmail.com>
2024-04-05 20:17:26 +0200

    Add session management

--- .config/nvim/lua/me/modules/tools/plugins.lua
+++ .config/nvim/lua/me/modules/tools/plugins.lua
@@ -14,4 +14,18 @@ return export {
     },
     docs = { "danymat/neogen" },
     zoxide = { "nanotee/zoxide.vim" },
+    session = {
+        "folke/persistence.nvim",
+        event = "BufReadPre", -- only start session saving when an actual file was opened
+        config = {
+            vim.api.nvim_create_user_command(
+                "SessionLoad",
+                function() require("persistence").load() end, {}
+            ),
+            vim.api.nvim_create_user_command(
+                "SessionRestore",
+                function() require("persistence").load { last = true } end, {}
+            ),
+        },
+    },
 }
