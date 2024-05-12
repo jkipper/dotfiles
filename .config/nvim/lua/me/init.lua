cbfb62d5d9d8ceee337fa939788177b31db70418
jkipper <justin.kipper94@gmail.com>
2024-05-12 13:44:24 +0200

    add back auto luafile

--- .config/nvim/lua/me/init.lua
+++ .config/nvim/lua/me/init.lua
@@ -15,3 +15,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("me.plugins", {})
 require "me.keymap"
+
+local Hooks = vim.api.nvim_create_augroup("Hooks", { clear = true })
+local enable_auto_luafile = function()
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "*.lua",
+        callback = function()
+            ---@diagnostic disable-next-line: missing-parameter
+            dofile(vim.fn.expand "%")
+        end,
+        group = Hooks,
+    })
+end
+vim.api.nvim_create_user_command("EnableAutoLuafile", enable_auto_luafile, {})
