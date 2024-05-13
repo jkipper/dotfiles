92e9b7df48a0a6470b2a11ba924c0f8524a013bc
jkipper <justin.kipper94@gmail.com>
2024-05-13 17:15:29 +0200

    go anchor yaml

--- .config/nvim/queries/go/injections.scm
+++ .config/nvim/queries/go/injections.scm
@@ -5,3 +5,13 @@
         (#set! injection.language "yaml")
     )
 )
+
+(var_declaration 
+  (var_spec 
+    name:  (identifier) @name (#match? @name ".*Yaml")
+    type: (type_identifier) 
+    value: (expression_list (raw_string_literal) @injection.content 
+        (#offset! @injection.content 0 1 0 -1) 
+        (#set! injection.language "yaml")
+    )
+))
