62f1752219fafb2a07d6a2b33387e287f076e0eb
jkipper <justin.kipper94@gmail.com>
2024-05-21 15:08:16 +0200

    Update yaml query

--- .config/nvim/queries/go/injections.scm
+++ .config/nvim/queries/go/injections.scm
@@ -1,6 +1,7 @@
 (short_var_declaration 
-    left: (expression_list (identifier) @name (#match? @name ".*Yaml"))
+    left: (expression_list (identifier))
     right: (expression_list (raw_string_literal) @injection.content 
+        (#match? @injection.content "^(`\n)?[a-zA-Z0-9_:\-]+:\n")
         (#offset! @injection.content 0 1 0 -1) 
         (#set! injection.language "yaml")
     )
@@ -8,9 +9,10 @@
 
 (var_declaration 
   (var_spec 
-    name:  (identifier) @name (#match? @name ".*Yaml")
+    name:  (identifier)
     type: (type_identifier) 
     value: (expression_list (raw_string_literal) @injection.content 
+        (#match? @injection.content "^(`\n)?[a-zA-Z0-9_:\-]+:\n")
         (#offset! @injection.content 0 1 0 -1) 
         (#set! injection.language "yaml")
     )
